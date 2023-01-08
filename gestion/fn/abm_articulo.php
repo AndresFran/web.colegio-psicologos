bea5e2467c545ad2726760fa67e98ca8919e88e1
cesarmario <cesarmariosdelosrios@gmail.com>
2023-01-08 13:00:25 -0300

    Update abm_articulo.php

--- gestion/fn/abm_articulo.php
+++ gestion/fn/abm_articulo.php
@@ -37,4 +37,50 @@ if ($_REQUEST['abm'] == 'a') {
             alert("Ocurrio un Error a guardar en la Base de Datos!!");
         </script>
 <?PHP }
-}
+} ?>
+
+<!-- Activar de Inmueble -->
+<?PHP
+if ($_REQUEST['abm'] == 'm') {
+    $query = "UPDATE novedad  SET
+    tituloNovedad='$_REQUEST[tituloNovedad]',
+    fechaNovedad='$_REQUEST[fechaNovedad]',
+    detalleNovedad='$_REQUEST[detalleNovedad]',
+    tipoNovedad='$_REQUEST[tipoNovedad]' WHERE idNovedad = '$_REQUEST[idNovedad]' ";
+} ?>
+
+<!-- Baja de Inmueble -->
+<?PHP
+if ($_REQUEST['abm'] == 'b') {
+    $query = "UPDATE novedad SET baja='1' WHERE idNovedad ='$_REQUEST[idNovedad]'";
+    $result = mysqli_query($conexion, $query);
+    if (mysqli_affected_rows($conexion) > 0) {     ?>
+        <script>
+            alert("Inmueble Eliminado correctamente");
+            location.replace("../index.php");
+        </script>
+    <?PHP } else { ?>
+        <script>
+            alert("Ocurrio un Error!!");
+            location.replace("../index.php");
+        </script>
+    <?PHP }; ?>
+<?PHP }; ?>
+
+<!-- Activar de Inmueble -->
+<?PHP
+if ($_REQUEST['abm'] == 'r') {
+    $query = "UPDATE novedad SET baja='0' WHERE idNovedad ='$_REQUEST[idNovedad]'";
+    $result = mysqli_query($conexion, $query);
+    if (mysqli_affected_rows($conexion) > 0) {     ?>
+        <script>
+            alert("Inmueble Activado correctamente");
+            location.replace("../index.php");
+        </script>
+    <?PHP } else { ?>
+        <script>
+            alert("Ocurrio un Error!!");
+            location.replace("../index.php");
+        </script>
+    <?PHP }; ?>
+<?PHP }; ?>
